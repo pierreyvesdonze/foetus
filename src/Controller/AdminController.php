e703081bee554396b87398ddf03b213678ae46fd
py <pyd3.14@gmail.com>
2021-05-12 13:01:47 +0200

    Ajout d'un formulaire d'upload de logo

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\Type\LogoType;
+use App\Service\ImageManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
@@ -22,5 +25,36 @@ class AdminController extends AbstractController
         } else {
             return $this->redirectToRoute('login');
         }
+    }
+
+    /**
+     * @Route("/admin/update/logo", name="update_logo")
+     * 
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function updateLogo(
+        Request $request,
+        ImageManager $imageManager
+        )
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $form = $this->createForm(LogoType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $photo = $form->get('logo')->getData();
+            if ($photo) {
+                $photoFileName = $imageManager->upload($photo, 'logo');
+                $imageManager->resize($photoFileName);
+
+            }
+
+            $this->addFlash('success', 'Logo modifié !');
+        }
+
+        return $this->render('admin/update.logo.html.twig', [
+            'form'  => $form->createView()
+        ]);
     }
 }
